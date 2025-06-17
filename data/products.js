ada85fe03419502dd27e2bd8d7c6844ef10617f9
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-06-17 13:50:16 -0500

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -89,6 +89,34 @@ const object3 = {
 object3.method();
 */
 
+export let products = [];
+
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun(); //ეს ფუნქცია მოდის amazon.js-დან როგორც პარამეტრი, და მერე აქ ვიძახებთ
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -754,3 +782,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
